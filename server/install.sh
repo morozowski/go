68c997887671c8fc5c0da4c9b821feed80d66c63
Luciano Morozowski Junior <morozowski@gmail.com>
2023-10-27 15:51:54 -0300

    ci: installation script

---
+++ server/install.sh
@@ -0,0 +1,17 @@
+#/usr/bin/bash
+echo "######### Instalando asdf #########"
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+echo "" >> ~/.bashrc
+echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
+echo "" >> ~/.bashrc
+. ~/.bashrc
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+
+echo "######### Instalando go #########"
+asdf install golang latest
+asdf global golang latest
+go version
+
+echo "######### Compilando #########"
+go build go/server/dynamic/dynamic.go
