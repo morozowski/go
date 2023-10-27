466c017b94d6ac5284aae055b6f77f0ace85cdcb
Luciano Morozowski Junior <morozowski@gmail.com>
2023-10-27 12:20:52 -0300

    Recebendo a porta na linha de comando

--- server/server.go
+++ server/server.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		log.Fatal("Missing parameter: <port>")
+	}
+	port := args[0]
+
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
 
-	log.Println("Executando...")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Println("Listening to port " + port + "...")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
