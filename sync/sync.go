8069514df7cccb7144a051d7855bdea0fd69494d
morozowski <morozowski@gmail.com>
2024-01-04 15:26:28 -0300

    concluído - funcionando no Windows

--- sync/sync.go
+++ sync/sync.go
@@ -8,12 +8,15 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"syscall"
 	"time"
 )
 
 var sourceDir string
 var targetDir string
+var debug bool = true
 
 func main() {
 	args := os.Args[1:]
@@ -25,72 +28,125 @@ func main() {
 	sourceDir = strings.ReplaceAll(args[0], "\\", "/")
 	targetDir = strings.ReplaceAll(args[1], "\\", "/")
 
-	log.Println("Walking directory " + sourceDir + "...")
-	err := filepath.WalkDir(sourceDir, visit)
+	abs, err := filepath.Abs(sourceDir)
+	check(err)
+
+	log.Println("Walking directory " + abs + "...")
+	// err = filepath.WalkDir(sourceDir, print)
+	err = filepath.WalkDir(sourceDir, sync)
 	check(err)
 }
 
-func visit(source string, d fs.DirEntry, err error) error {
+type fileDetails struct {
+	Info  fs.FileInfo
+	Attrs uint32
+}
+
+func getDetails(path string) *fileDetails {
+	info, err := os.Stat(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	check(err)
+
+	attrs := getFileAttributes(path)
+
+	return &fileDetails{
+		Info:  info,
+		Attrs: attrs,
+	}
+}
+
+func print(source string, d fs.DirEntry, err error) error {
 	if err != nil {
 		return err
 	}
-	sourceInfo, err := os.Stat(source)
+	rel, err := filepath.Rel(sourceDir, source)
 	check(err)
+	target := filepath.Join(targetDir, rel)
+
+	sourceDetails := getDetails(source)
+	targetDetails := getDetails(target)
+
+	if targetDetails != nil {
+		printFilesDiff("", rel, *sourceDetails, *targetDetails)
+	}
+
+	return nil
+}
+
+func sync(source string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	sourceDetails := getDetails(source)
 
 	rel, err := filepath.Rel(sourceDir, source)
 	check(err)
 
 	target := filepath.Join(targetDir, rel)
+	targetDetails := getDetails(target)
+	newTargetDetails := targetDetails
 	if d.Type().IsDir() {
-		targetInfo, err := os.Stat(target)
-		if errors.Is(err, os.ErrNotExist) {
+		if debug {
+			log.Printf("Walking directory %s...\n", rel)
+		}
+		if targetDetails == nil {
 			log.Printf("Creating directory %s\n", rel)
-			err := os.Mkdir(target, sourceInfo.Mode())
+			err := os.Mkdir(target, sourceDetails.Info.Mode())
 			check(err)
 
-			updateModTime(target, sourceInfo.ModTime())
-			printFilesDiffAfterwards(rel, sourceInfo, target)
+			updateModTime(target, sourceDetails.Info.ModTime())
+			newTargetDetails = printFilesDiffAfterwards(rel, *sourceDetails, target)
 		} else {
-			check(err)
-			if !targetInfo.IsDir() {
+			if !targetDetails.Info.IsDir() {
 				log.Fatal(target + " should be a directory!")
 			} else { // target directory is newer: sync the modification date
-				deltaTime := sourceInfo.ModTime().Sub(targetInfo.ModTime())
+				deltaTime := sourceDetails.Info.ModTime().Sub(targetDetails.Info.ModTime())
 				if deltaTime > 100 || deltaTime <= -1000000000 {
-					printFilesDiff("BEFORE", rel, sourceInfo, targetInfo)
+					printFilesDiff("BEFORE", rel, *sourceDetails, *targetDetails)
 					log.Printf("Updating modification time of directory %s\n", rel)
-					updateModTime(target, sourceInfo.ModTime())
-					printFilesDiffAfterwards(rel, sourceInfo, target)
+					updateModTime(target, sourceDetails.Info.ModTime())
+					newTargetDetails = printFilesDiffAfterwards(rel, *sourceDetails, target)
 				}
 			}
 		}
 	} else {
-		targetInfo, err := os.Stat(target)
-		if errors.Is(err, os.ErrNotExist) {
+		if targetDetails == nil {
 			log.Printf("Copying file %s\n", rel)
-			syncFile(source, target, sourceInfo)
-			printFilesDiffAfterwards(rel, sourceInfo, target)
+			syncFile(source, target, sourceDetails.Info)
+			newTargetDetails = printFilesDiffAfterwards(rel, *sourceDetails, target)
 		} else {
 			check(err)
-			if !targetInfo.Mode().IsRegular() {
+			if !targetDetails.Info.Mode().IsRegular() {
 				log.Fatal(target + " should be a regular file!")
 			}
 
-			deltaTime := sourceInfo.ModTime().Sub(targetInfo.ModTime())
-			deltaSize := sourceInfo.Size() - targetInfo.Size()
-			if (deltaTime > 100 || deltaTime <= -1000000000) || deltaSize != 0 {
-				printFilesDiff("BEFORE", rel, sourceInfo, targetInfo)
+			deltaTime := sourceDetails.Info.ModTime().Sub(targetDetails.Info.ModTime())
+			deltaSize := sourceDetails.Info.Size() - targetDetails.Info.Size()
+			if (deltaTime > 100 || deltaTime <= -1000000000) || deltaSize != 0 || sourceDetails.Attrs != targetDetails.Attrs {
+				printFilesDiff("BEFORE", rel, *sourceDetails, *targetDetails)
 				if deltaTime > 100 || deltaSize != 0 {
 					log.Printf("Updating file %s\n", rel)
-					syncFile(source, target, sourceInfo)
-					printFilesDiffAfterwards(rel, sourceInfo, target)
-				} else { // same size, target file is newer: sync the modification date
+					if isWindowsHiddenFile(target) {
+						log.Println("Hidden file cannot be updated in Windows: removing it...")
+						os.Remove(target)
+					}
+					syncFile(source, target, sourceDetails.Info)
+					newTargetDetails = printFilesDiffAfterwards(rel, *sourceDetails, target)
+				} else if deltaTime <= -1000000000 { // same size, target file is newer: sync the modification date
 					log.Printf("Updating modification time of file %s\n", rel)
-					updateModTime(target, sourceInfo.ModTime())
-					printFilesDiffAfterwards(rel, sourceInfo, target)
+					updateModTime(target, sourceDetails.Info.ModTime())
+					newTargetDetails = printFilesDiffAfterwards(rel, *sourceDetails, target)
 				}
 			}
 		}
+	}
+	if sourceDetails.Attrs != newTargetDetails.Attrs {
+		printFilesDiff("BEFORE", rel, *sourceDetails, *newTargetDetails)
+		log.Printf("Updating attributes of file %s\n", rel)
+		setFileAttributes(target, sourceDetails.Attrs)
+		_ = printFilesDiffAfterwards(rel, *sourceDetails, target)
 	}
 	return nil
 }
@@ -101,20 +157,91 @@ func updateModTime(filePath string, modTime time.Time) {
 	check(err)
 }
 
-func printFilesDiffAfterwards(fileName string, sourceInfo fs.FileInfo, target string) fs.FileInfo {
-	newTargetInfo, err := os.Stat(target)
-	check(err)
-	printFilesDiff("AFTER", fileName, sourceInfo, newTargetInfo)
-	return newTargetInfo
+func printFilesDiffAfterwards(fileName string, sourceDetails fileDetails, target string) *fileDetails {
+	targetDetails := getDetails(target)
+	printFilesDiff("AFTER", fileName, sourceDetails, *targetDetails)
+	return targetDetails
 }
 
-func printFilesDiff(message, fileName string, sourceInfo, targetInfo fs.FileInfo) {
-	log.Printf("%s\nFile: %s\nSource: %-40v %12d bytes %12v\nTarget: %-40v %12d bytes %12v\nΔT: %d",
-		message,
-		fileName,
-		sourceInfo.ModTime(), sourceInfo.Size(), sourceInfo.Mode(),
-		targetInfo.ModTime(), targetInfo.Size(), targetInfo.Mode(),
-		sourceInfo.ModTime().Sub(targetInfo.ModTime()))
+func printFilesDiff(message, fileName string, sourceDetails, targetDetails fileDetails) {
+	if debug {
+		sourceMode := sourceDetails.Info.Mode()
+		targetMode := targetDetails.Info.Mode()
+		log.Printf("%s\nFile: %s\nSource: %-40v %12d bytes %12v %16b %s\nTarget: %-40v %12d bytes %12v %16b %s\nΔT: %d",
+			message,
+			fileName,
+			sourceDetails.Info.ModTime(), sourceDetails.Info.Size(), sourceMode, uint32(sourceDetails.Attrs), detailed(sourceDetails.Attrs),
+			targetDetails.Info.ModTime(), targetDetails.Info.Size(), targetMode, uint32(targetDetails.Attrs), detailed(targetDetails.Attrs),
+			sourceDetails.Info.ModTime().Sub(targetDetails.Info.ModTime()))
+	}
+}
+
+func detailed(attrs uint32) string {
+	var buf strings.Builder
+	if attrs&syscall.FILE_ATTRIBUTE_READONLY != 0 {
+		buf.WriteString(" RO")
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_HIDDEN != 0 {
+		buf.WriteString(" H")
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_SYSTEM != 0 {
+		buf.WriteString(" S")
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_DIRECTORY != 0 {
+		buf.WriteString(" D")
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_ARCHIVE != 0 {
+		buf.WriteString(" A")
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_NORMAL != 0 {
+		buf.WriteString(" N")
+	}
+	if attrs&syscall.FILE_ATTRIBUTE_REPARSE_POINT != 0 {
+		buf.WriteString(" RP")
+	}
+	return buf.String()
+}
+
+// https://freshman.tech/snippets/go/detect-hidden-file/
+// isHidden checks if a file is hidden on Windows.
+func isWindowsHiddenFile(path string) bool {
+	if runtime.GOOS != "windows" {
+		return false
+	}
+
+	return getFileAttributes(path)&syscall.FILE_ATTRIBUTE_HIDDEN != 0
+}
+
+func getFileAttributes(path string) uint32 {
+	absPath, err := filepath.Abs(path)
+	check(err)
+
+	// Appending `\\?\` to the absolute path helps with
+	// preventing 'Path Not Specified Error' when accessing
+	// long paths and filenames
+	// https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=cmd
+	pointer, err := syscall.UTF16PtrFromString(`\\?\` + absPath)
+	check(err)
+
+	attributes, err := syscall.GetFileAttributes(pointer)
+	check(err)
+
+	return attributes & 0xFF
+}
+
+func setFileAttributes(path string, attributes uint32) {
+	absPath, err := filepath.Abs(path)
+	check(err)
+
+	// Appending `\\?\` to the absolute path helps with
+	// preventing 'Path Not Specified Error' when accessing
+	// long paths and filenames
+	// https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=cmd
+	pointer, err := syscall.UTF16PtrFromString(`\\?\` + absPath)
+	check(err)
+
+	err = syscall.SetFileAttributes(pointer, attributes)
+	check(err)
 }
 
 func syncFile(source string, target string, sourceInfo fs.FileInfo) {
@@ -143,6 +270,7 @@ func copyFile(source, target string, sourceInfo fs.FileInfo) int64 {
 
 func check(e error) {
 	if e != nil {
-		log.Fatal(e)
+		panic(e)
+		// log.Fatal(e)
 	}
 }
